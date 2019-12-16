dd73468c620bd9b2ae3a3bcff9227b6b9af22a7f
Angela Weber <angelaweber95@hotmail.com>
2019-12-15 22:42:39 -0800

    complete weekend lesson

---
+++ lib/album.rb
@@ -0,0 +1,39 @@
+class Album
+  attr_reader :id, :name
+  @@albums = {}
+  @@total_rows = 0
+
+  def initialize(name, id)
+    @name = name
+    @id = id || @@total_rows += 1
+  end
+
+  def self.all
+    @@albums.values()
+  end
+
+  def save
+    @@albums[self.id] = Album.new(self.name, self.id)
+  end
+
+  def ==(album_to_compare)
+    self.name() == album_to_compare.name()
+  end
+
+  def self.clear
+    @@albums = {}
+    @@total_rows = 0
+  end
+
+  def self.find(id)
+    @@albums[id]
+  end
+
+  def update(name)
+    @name = name
+  end
+
+  def delete
+    @@albums.delete(self.id)
+  end
+end
